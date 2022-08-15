d2effc5fdcb97524a475150686bca9007aa4f60d
meta93 <bismarkkyeremateng93@gmail.com>
2022-08-15 15:15:22 +0000

    javascript days

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-alert("😓😭");
+const subbt = document.querySelector(".sub-button");
+const selwrap = document.querySelector(".class-wrapper");
+const wrap_container = document.querySelector(".container");
+con;
+selwrap.addEventListener("click", (w) => {
+  w.preventDefault();
+  document.querySelector(".class-wrapper").style.background = "#FC7614";
+});
