75b73d8cbb6b11db861158e923164e24fb96208d
meta93 <bismarkkyeremateng93@gmail.com>
2022-08-16 00:16:34 +0000

    work

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const subbt = document.querySelector(".sub-button");
 const selwrap = document.querySelector(".class-wrapper");
 const wrap_container = document.querySelector(".container");
-con;
-selwrap.addEventListener("click", (w) => {
-  w.preventDefault();
-  document.querySelector(".class-wrapper").style.background = "#FC7614";
+const select_wrapper = document.querySelector(".select-wrapper");
+const ratings = document.querySelector('.rate');
+
+ratings.forEach(rate => {
+ ratings.addEventListener (rate)
+  rate.preventDefault();
+  this.classist.add(".selected");
 });
+  
